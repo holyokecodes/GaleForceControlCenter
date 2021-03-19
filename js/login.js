e04c10da7f0e37bf12b5699122be6dc5ef2e3fcb
Jacob Bashista <tgb.bashista@gmail.com>
2021-03-19 13:42:33 -0400

    Remove Team Name from Login Screen

--- js/login.js
+++ js/login.js
@@ -4,20 +4,17 @@ $(() => {
     window.vuplex.addEventListener('message', function messageEndListener(event) {
         let json = JSON.parse(event.data);
         $('#displayNameInput').val(json.DisplayName);
-        $('#teamNameInput').val(json.TeamName);
         $('#shirtColorInput').val(json.ShirtColor);
         $('#pantsColorInput').val(json.PantsColor);
     });
 
     $('#loginButton').click(() => {
         let displayName = $('#displayNameInput').val();
-        let teamName = $('#teamNameInput').val();
         let shirtColor = $('#shirtColorInput').val();
         let pantsColor = $('#pantsColorInput').val();
 
         var jsonObj = {
             DisplayName: displayName,
-            TeamName: teamName,
             ShirtColor: shirtColor,
             PantsColor: pantsColor
         }
