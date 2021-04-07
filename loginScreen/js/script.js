f35dad4e04e68a23ed2ebec4b7d1362229196432
Andrew Pasquale <andrew@elytra.net>
2021-04-06 22:13:55 -0400

    fix case typo

--- loginScreen/js/script.js
+++ loginScreen/js/script.js
@@ -6,9 +6,9 @@ document.addEventListener('DOMContentLoaded', () => {
         window.vuplex.addEventListener('message', function messageEndListener(event) {
             let json = JSON.parse(event.data);
             console.log(JSON.stringify(json));
-            document.getElementById('hairColor').jscolor.fromString('#00FF00');
+            document.getElementById('hairColor').jscolor.fromString(json.HairColor);
             document.getElementById('skinColor').jscolor.fromString(json.SkinColor);
-            document.getElementById('pantsColor').jscolor.fromString(json.pantsColor);
+            document.getElementById('pantsColor').jscolor.fromString(json.PantsColor);
         });
     }
 
